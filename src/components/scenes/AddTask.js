b63fd5bb77ea4621fd28e019a983e8249fc5fdf1
hannalikhtman <h.likhtman@gmail.com>
2018-09-01 15:23:49 +0300

    add form and apply to actions

--- src/components/scenes/AddTask.js
+++ src/components/scenes/AddTask.js
@@ -1,13 +1,54 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import { View, TextInput } from 'react-native'
+import { Button, FormLabel, FormInput } from 'react-native-elements'
 import { connect } from 'react-redux'
 import { titleChanged, descriptionChanged, typeChanged } from '../../redux/actions'
 
 class AddTask extends Component {
+
+  titleChange = (text) => {
+    this.props.titleChanged(text)
+  }
+
+  descriptionChange = (text) => {
+    this.props.descriptionChanged(text)
+  }
+
+  typeChange = (text) => {
+    this.props.typeChanged(text)
+  }
+
   render() {
     return (
       <View>
-        <Text>Add</Text>
+        <FormLabel>Title</FormLabel>
+        <FormInput
+          value={this.props.title}
+          placeholder="Please type a title of your task..."
+          onChangeText={this.titleChange}
+        />
+
+        <FormLabel>Description</FormLabel>
+        <FormInput
+          value={this.props.description}
+          placeholder="Please add some description..."
+          onChangeText={this.descriptionChange}
+        />
+
+        <FormLabel>Type</FormLabel>
+        <FormInput
+          value={this.props.type}
+          placeholder="Please chose a category of task..."
+          onChangeText={this.typeChange}
+        />
+
+        <Button
+          backgroundColor="black"
+          textStyle={{ color: 'yellow', fontWeight: 'bold' }}
+          title="Add task"
+          style={{ marginTop: 20 }}
+          onPress={() => {}}
+        />
       </View>
     )
   }
